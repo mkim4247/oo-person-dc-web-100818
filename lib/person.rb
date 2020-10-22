fd97783fa19d024f25c8e12deb859dd1ed267aff
Michael Kim <m.kim4247@gmail.com>
2020-10-22 14:54:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,14 @@
-require 'pry'
-
 class Person
   attr_reader :name
-  attr_accessor :happiness, :hygiene
-
+  attr_accessor :bank_account, :happiness, :hygiene
+  
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-
+  
   def clean?
     if @hygiene > 7
       true
@@ -18,7 +16,7 @@ class Person
       false
     end
   end
-
+  
   def happy?
     if @happiness > 7
       true
@@ -26,73 +24,59 @@ class Person
       false
     end
   end
-
+  
+  def happiness=(points)
+    @happiness = points
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    end
+  end
+  
+  def hygiene=(points)
+    @hygiene = points
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    end
+  end
+  
   def get_paid(salary)
     @bank_account += salary
     "all about the benjamins"
-    @bank_account
   end
-
+  
   def take_bath
-    @hygiene += 4
-
-    if @hygiene > 10
-      @hygiene = 10
-    end
-
+    self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
-
   end
-
+  
   def work_out
-    @happiness += 2
-    @hygiene -= 3
-
-    if @happiness > 10
-      @happiness = 10
-    end
-
-    if @hygiene < 0
-      @hygiene = 0
-    end
-
+    self.happiness += 2
+    self.hygiene -= 3
     "♪ another one bites the dust ♫"
-
   end
-
-    def happier(num)
-      @happiness += num
-      if @happiness > 10
-        @happiness = 10
-      end
+  
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+  
+  def start_conversation(person, topic)
+    if topic == 'politics'
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == 'weather'
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
     end
-
-    def sadder(num)
-      @happiness -= num
-      if @happiness < 0
-        @happiness = 0
-      end
-    end
-
-    def call_friend(friend)
-      self.happier(3)
-      friend.happier(3)
-      "Hi #{friend.name}! It's #{self.name}. How are you?"
-    end
-
-    def start_conversation(friend, topic)
-        if topic == "politics"
-          self.sadder(2)
-          friend.sadder(2)
-          "blah blah partisan blah lobbyist"
-        elsif topic == "weather"
-          self.happier(1)
-          friend.happier(1)
-          "blah blah sun blah rain"
-        else
-          @happiness
-          "blah blah blah blah blah"
-        end
-    end
-binding.pry
+  end
+  
 end
